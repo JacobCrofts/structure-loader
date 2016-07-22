c3051a0708838140b676403da66695db46bfcb9f
John Doe <johndoe@example.com>
2016-07-22 14:33:13 -0500

    Initial commit

---
+++ src/me/jacobcrofts/simplestructureloader/SimpleStructureLoader.java
@@ -0,0 +1,17 @@
+package me.jacobcrofts.simplestructureloader;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpleStructureLoader extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
