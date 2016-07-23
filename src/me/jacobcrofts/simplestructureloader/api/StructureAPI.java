9a66323eb953fe8efe1c3fd6a3d8046b01ba41fa
John Doe <johndoe@example.com>
2016-07-22 19:24:58 -0500

    Add feature: API class that writes selection to file

--- src/me/jacobcrofts/simplestructureloader/api/StructureAPI.java
+++ src/me/jacobcrofts/simplestructureloader/api/StructureAPI.java
@@ -1,13 +1,24 @@
 package me.jacobcrofts.simplestructureloader.api;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import me.jacobcrofts.simplestructureloader.util.Selection;
 
 public final class StructureAPI {
 	
 	private StructureAPI() {}
 	
-	public static void writeToFile(String path, Selection selection) {
-		
+	public static void writeToFile(String path, Selection selection) throws IOException {
+		FileWriter writer = new FileWriter(path);
+		writer.write(selection.toJSON().toJSONString());
+		writer.close();
 	}
+	
+//	playerData = (JSONObject) new JSONParser().parse(new FileReader(f));
 
 }
