9c01638687b7d6bcafd874d5d593877b2c18fe3e
John Doe <johndoe@example.com>
2016-07-22 19:18:49 -0500

    Add feature: upon login, player is registered

---
+++ src/me/jacobcrofts/simplestructureloader/api/StructureAPI.java
@@ -0,0 +1,13 @@
+package me.jacobcrofts.simplestructureloader.api;
+
+import me.jacobcrofts.simplestructureloader.util.Selection;
+
+public final class StructureAPI {
+	
+	private StructureAPI() {}
+	
+	public static void writeToFile(String path, Selection selection) {
+		
+	}
+
+}
