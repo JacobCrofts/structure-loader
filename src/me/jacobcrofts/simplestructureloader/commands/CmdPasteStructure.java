1c41ec55e850426f2d13cb8d403255a1b7622f79
John Doe <johndoe@example.com>
2016-07-24 13:38:03 -0500

    Add feature: error handling for paste command

--- src/me/jacobcrofts/simplestructureloader/commands/CmdPasteStructure.java
+++ src/me/jacobcrofts/simplestructureloader/commands/CmdPasteStructure.java
@@ -2,6 +2,8 @@ package me.jacobcrofts.simplestructureloader.commands;
 
 import java.io.IOException;
 
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,15 +18,22 @@ public class CmdPasteStructure implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		
-		if (sender instanceof Player) {			
-			Selection selection;
-			try {
-				selection = new Selection(StructureAPI.readFromFile("plugins/structures/" + args[0] + ".json"));
-				StructureAPI.placeStructure(selection, ((Player) sender).getLocation());
-			} catch (IOException | ParseException e) {
-				e.printStackTrace();
+		if (sender instanceof Player) {
+			Player player = (Player) sender;
+			if (player.getGameMode() == GameMode.CREATIVE && player.isOp()) {				
+				Selection selection;
+				try {
+					selection = new Selection(StructureAPI.readFromFile("plugins/structures/" + args[0] + ".json"));
+					StructureAPI.placeStructure(selection, player.getLocation());
+				} catch (IOException | ParseException e) {
+					e.printStackTrace();
+				}
+			} else {
+				player.sendMessage(ChatColor.RED + "You do not have permission to execute this command.");
 			}
 			return true;
+		} else {
+			sender.sendMessage("Only players may perform this command.");
 		}
 		
 		return false;
